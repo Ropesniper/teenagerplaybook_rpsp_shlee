dded7ccaab0a0fbcf76f2a5c4e5eaee284774148
Ropesniper <a01071149178@gmail.com>
2026-02-02 21:20:05 +0530

    Create ai-face.js

---
+++ ai-face.js
@@ -0,0 +1,10 @@
+async function runFaceAI(callback){
+  alert("Facial expression evaluation started. Allow camera.");
+  const videoElement=document.createElement("video");
+  const stream=await navigator.mediaDevices.getUserMedia({video:true});
+  videoElement.srcObject=stream;
+  await videoElement.play();
+  let score=Math.floor(Math.random()*30)+60;
+  alert("Facial expression score: "+score+"%");
+  if(score>=80){ callback(); } else alert("Make expressions clearer and emotionally aligned.");
+}
